e1855b4a2adc2bf622e81ab7cb19d555492f1817
Francis <bilodeaurobillardfrancis@gmail.com>
2020-04-05 09:00:44 -0400

    Refactoring to make python file PEP8 compliant

--- projects/02_trivia_api/starter/backend/test_flaskr.py
+++ projects/02_trivia_api/starter/backend/test_flaskr.py
@@ -15,7 +15,9 @@ class TriviaTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "trivia_test"
-        self.database_path = "postgres://{}@{}/{}".format('postgres:61785','localhost:5432', self.database_name)
+        self.database_path = "postgres://{}@{}/{}".format('postgres: 61785',
+                                                          'localhost: 5432',
+                                                          self.database_name)
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -26,8 +28,9 @@ class TriviaTestCase(unittest.TestCase):
             self.db.create_all()
 
         self.new_question = {
-            'question':'What year was the first model of the iPhone released?',
-            'answer':'2007',
+            'question': 'What year was the first model
+            of the iPhone released?',
+            'answer': '2007',
             'category': 4,
             'difficulty': 1
         }
@@ -37,7 +40,7 @@ class TriviaTestCase(unittest.TestCase):
                 'id': 2,
                 'type': 'Art'
             },
-            'previous_questions':[16]
+            'previous_questions': [16]
         }
 
         self.quiz_data_all = {
@@ -45,9 +48,9 @@ class TriviaTestCase(unittest.TestCase):
                 'id': 0,
                 'type': 'ALL'
             },
-            'previous_questions':[10]
+            'previous_questions': [10]
         }
-    
+
     def tearDown(self):
         """Executed after each test"""
         pass
@@ -73,7 +76,7 @@ class TriviaTestCase(unittest.TestCase):
 
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
-    
+
     def test_create_new_question(self):
         res = self.client().post('/questions/add', json=self.new_question)
         data = json.loads(res.data)
@@ -81,7 +84,7 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
         self.assertTrue(data['created'])
-    
+
     def test_delete_question(self):
         res = self.client().delete('/questions/12')
         data = json.loads(res.data)
@@ -93,25 +96,23 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(data['deleted'], 12)
         self.assertEqual(question, None)
 
-
     def test_422_delete_question_bd_error(self):
         res = self.client().delete('/questions/140000')
 
         self.assertEqual(res.status_code, 422)
-        
-    
+
     def test_search_questions(self):
-        res = self.client().post('/questions', json= {'searchTerm':"title"})
+        res = self.client().post('/questions', json={'searchTerm': "title"})
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
         self.assertTrue(len(data['questions']))
         self.assertEqual(len(data['questions']), 2)
-    
+
     def test_list_questions_from_category(self):
         res = self.client().get('/categories/3/questions')
-        data = json.loads(res.data) 
+        data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
@@ -124,22 +125,24 @@ class TriviaTestCase(unittest.TestCase):
 
         self.assertEqual(res.status_code, 404)
         self.assertEqual(data['success'], False)
-    
+
     def test_get_questions_for_quiz_with_category(self):
-        res = self.client().post('/quiz/play', json = self.quiz_data_art)
+        res = self.client().post('/quiz/play', json=self.quiz_data_art)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
-        self.assertTrue(data['question']['id'] not in self.quiz_data_art['previous_questions'])
+        self.assertTrue(data['question']['id'] not in
+                        self.quiz_data_art['previous_questions'])
 
     def test_get_questions_for_quiz_without_category(self):
-        res = self.client().post('/quiz/play', json = self.quiz_data_all)
+        res = self.client().post('/quiz/play', json=self.quiz_data_all)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
-        self.assertTrue(data['question']['id'] not in self.quiz_data_art['previous_questions'])
+        self.assertTrue(data['question']['id'] not in
+                        self.quiz_data_art['previous_questions'])
 
 
 # Make the tests conveniently executable
